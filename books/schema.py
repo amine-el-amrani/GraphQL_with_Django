d3b997e790c4f7c5c8ed3e57007a300010f6e0be
El amrani amine <amine_el_amrani@outlook.com>
2024-03-18 09:42:18 +0100

    feat(api): setup GraphQL and develop the schema (#1)

---
+++ books/schema.py
@@ -0,0 +1,29 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+
+
+
+
+
+class BooksType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ("id", "title", "excerpt")
+
+
+
+class Query(graphene.ObjectType):
+
+    all_books = graphene.List(BooksType)
+
+    def resolve_all_books(self, info, **kwargs):
+        return Books.objects.all()
+
+
+    
+
+schema = graphene.Schema(query=Query)
+
+
