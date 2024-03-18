26b4819c2a72dac9728e1468cd69bfd0b268da69
El amrani Amine <amine_el_amrani@outlook.com>
2024-03-18 11:37:59 +0100

    feat(api): Add models for Category, Author, and Publisher

--- books/models.py
+++ books/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Books(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.TextField()
+    price = models.FloatField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=True, blank=True)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.title
